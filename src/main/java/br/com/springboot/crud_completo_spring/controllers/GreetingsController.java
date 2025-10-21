23c7b86849937bd9c1106b5be3ebab8e6c8994d3
Caio Filardis <caiosantosshake@hotmail.com>
2025-10-21 13:32:51 -0300

    Criando o Repository com Spring Data

--- src/main/java/br/com/springboot/crud_completo_spring/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/crud_completo_spring/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.crud_completo_spring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.crud_completo_spring.model.Usuario;
+import br.com.springboot.crud_completo_spring.repository.UsuarioRepository;
+
 @RestController // permite a realização de requisições REST
 public class GreetingsController {
 
+	// declarar o usuário repository
+	@Autowired(required=true) // CD ou CDI -> indica a injeção de dependência
+	private UsuarioRepository usuarioRepository;
+	
+	
     //@RequestMapping(value = "/{name}", method = RequestMethod.GET) // URL padrão '{}' significa que é uma variável
 	@RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET) // URL específica(modificada por mim)
     @ResponseStatus(HttpStatus.OK)
@@ -22,6 +31,11 @@ public class GreetingsController {
 	@ResponseStatus(HttpStatus.OK) // tipo de resposta esperado
 	public String metodoRetornaString(@PathVariable String umNome ) { // anotação do spring
 	
+		Usuario usuario = new Usuario(); // instancia o objeto
+		usuario.setName(umNome);
+		
+		usuarioRepository.save(usuario); // grava no banco
+		
 		return "Olá Mundo: " + umNome;
 	}
 }
