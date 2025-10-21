e76c988efaf3c43e0a435eb239a4903122a53302
Caio Filardis <caiosantosshake@hotmail.com>
2025-10-21 13:02:21 -0300

    Criando nossa primeira entidade e tabela na nossa aplicação.

---
+++ src/main/java/br/com/springboot/crud_completo_spring/model/Usuario.java
@@ -0,0 +1,58 @@
+package br.com.springboot.crud_completo_spring.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity // indica que é um entidade para o JPA
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+public class Usuario implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	private Long id;
+	
+	private String name;
+	private int idade;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	} 
+	
+	
+	
+}
