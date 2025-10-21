25b37300e58bfc0a4b7c4d0ac4d429d6d8f94608
Caio Filardis <caiosantosshake@hotmail.com>
2025-10-21 17:03:08 -0300

    Criando Endpoint para deletar do banco de dados. Resolvendo problemas de enconder do 'application.propeties' e ajustando parÂmetros de porta.

--- src/main/java/br/com/springboot/crud_completo_spring/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/crud_completo_spring/controllers/GreetingsController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +50,17 @@ public class GreetingsController {
 		// 2. retorna novo objeto salvo
 		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
 	}
+	
+	// método para deletar
+	@DeleteMapping(value="deletar")
+	@ResponseBody
+	public ResponseEntity<String> deletar(@RequestParam Long usuarioId) { // indica a passagem de parâmetro
+		
+		usuarioRepository.deleteById(usuarioId);
+		
+		return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+	}
+	
+	
 	
 }
